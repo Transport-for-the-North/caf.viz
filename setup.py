08d8f9ab4fde3d60cd535ace1167efa5e902f4c2
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2023-02-14 10:27:15 +0000

    package setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import versioneer
 
 if __name__ == "__main__":
     setup(
-        name="caf.{package_name}",
+        name="caf.viz",
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
     )
