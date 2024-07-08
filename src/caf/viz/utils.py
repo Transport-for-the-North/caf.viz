1a205248ffe9d7d9aaedded199a3a05bbf298c39
Matt Buckley <matt.buckley@transportforthenorth.com>
2024-07-08 15:44:58 +0100

    Fix - TfN style packaging
    Added tfn style file to Python package.
    Added function to find caf.viz matplotlib styles.

--- src/caf/viz/utils.py
+++ src/caf/viz/utils.py
@@ -5,12 +5,16 @@
 
 # Built-Ins
 import logging
+import pathlib
 import re
+
+# Third Party
+from matplotlib.style import core
 
 ##### CONSTANTS #####
 
 LOG = logging.getLogger(__name__)
-
+_PACKAGE_PATH = pathlib.Path(__file__).parent
 
 ##### CLASSES & FUNCTIONS #####
 
@@ -19,3 +23,22 @@ def normalise_name(name: str) -> str:
     """Convert name to lowercase and replace spaces with underscore."""
     name = name.lower().strip()
     return re.sub(r"\s+", "_", name)
+
+
+def style_names() -> list[str]:
+    """List current matplotlib styles available in CAF.viz.
+
+    Examples
+    --------
+    Listing all available caf.viz styles.
+    >>> from caf.viz import utils
+    >>> utils.style_names()
+    ['tfn']
+
+    Using the TfN style.
+    >>> from matplotlib import pyplot as plt
+    >>> plt.style.use("caf.viz.tfn")
+    """
+    styles = core.read_style_directory(_PACKAGE_PATH)
+
+    return list(styles)
