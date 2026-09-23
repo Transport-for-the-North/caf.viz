0607dba4434ed9f19115793ced4ca410c789ab65
Matt Buckley <matt.buckley@transportforthenorth.com>
2026-09-23 10:13:48 +0100

    Fix: ruff autofix

--- src/caf/viz/xy_plot.py
+++ src/caf/viz/xy_plot.py
@@ -317,7 +317,7 @@ def plot_xy(
     type_: XYPlotType = XYPlotType.SCATTER,
     *,
     title: str | None = None,
-    weight_column: None | str | Sequence[str] = None,
+    weight_column: str | Sequence[str] | None = None,
 ) -> figure.Figure:
     """Create a graph of `data` based on given columns.
 
